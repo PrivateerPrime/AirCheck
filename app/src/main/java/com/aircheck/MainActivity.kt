5943b0a282c4b809031edec547dfcb418c68170a
PrivateerPrime <kacperlipowicz@gmail.com>
2021-11-19 16:37:37 +0100

    Dodano pobieranie danych oraz ikonę odświeżenia

--- app/src/main/java/com/aircheck/MainActivity.kt
+++ app/src/main/java/com/aircheck/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.aircheck
 
 import android.os.Bundle
-import android.widget.ListView
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,10 +10,42 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.aircheck.databinding.ActivityMainBinding
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.HurlStack
+import com.android.volley.toolbox.StringRequest
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var requestQueue: RequestQueue;
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        val url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=39&appid=b06c7b5f09d3a79aa795615537b676c5"
+        val stringRequest = StringRequest(Request.Method.GET, url,
+            {
+                    response -> print(response)
+            }, {
+                    error ->  print(error)
+            }
+        )
+        try {
+            requestQueue.add(stringRequest)
+        }
+        catch (e: Exception) {
+            print(e.stackTrace)
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +62,24 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_settings, R.id.navigation_other))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val cache = DiskBasedCache(cacheDir, 1024*1024)
+        val network = BasicNetwork((HurlStack()))
+
+        requestQueue = RequestQueue(cache, network).apply {
+            start()
+        }
+
+//        val url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=39&appid=b06c7b5f09d3a79aa795615537b676c5"
+//
+//        val stringRequest = StringRequest(Request.Method.GET, url,
+//            {
+//                    response -> println(response)
+//            }, {
+//                    error ->  println(error)
+//            }
+//        )
+//        requestQueue.add(stringRequest)
     }
+
 }
