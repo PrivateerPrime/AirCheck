828ee0be43b005a98081d970fd205d006327fb8b
Kacper Lipowicz <kacperlipowicz@gmail.com>
2021-11-29 19:22:20 +0100

    Settings

--- app/src/main/java/com/aircheck/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/aircheck/ui/settings/SettingsFragment.kt
@@ -1,13 +1,17 @@
 package com.aircheck.ui.settings
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.aircheck.R
 import com.aircheck.databinding.FragmentSettingsBinding
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.google.android.material.slider.RangeSlider
 
 class SettingsFragment : Fragment() {
@@ -34,6 +38,9 @@ class SettingsFragment : Fragment() {
 //            textView.text = it
 //        })
 
+        val preferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = preferences?.edit()
+
         val rangeSlider: RangeSlider = binding.sliderRange
         val rangeTextView: TextView = binding.textSearchRange
 
@@ -46,8 +53,39 @@ class SettingsFragment : Fragment() {
 
         }
 
+        val units1Button: MaterialButtonToggleGroup = binding.toggleGroupUnits1
+        units1Button.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            if (isChecked){
+                when (checkedId) {
+                    binding.buttonKm.id -> {
+                        editor?.putString("Unit", "Km")
+                        Log.i("UNIT", "KM")
+                        editor?.apply()
+                    }
+                    binding.buttonMil.id -> {
+                        editor?.putString("Unit", "Mil")
+                        Log.i("UNIT", "Mil")
+                        editor?.apply()
+                    }
+                }
+
+            }
+        }
+        val kmButton: Button = binding.buttonKm
+        val milButton: Button = binding.buttonMil
+        val selection = preferences?.getString("Unit", null)
+        if(selection == "Mil")
+            units1Button.check(milButton.id);
+        else
+            units1Button.check(kmButton.id);
+
+//        editor?.putString("Temp", "Celc")
+//        editor?.putString("Lang", "ENG")
+//        editor?.apply()
+
         //TODO Dodanie zmiany języka
-        //https://developer.android.com/training/data-storage/shared-preferences
+        //https://developer.android.com/training/data-storage/shared-
+//        https://developer.android.com/guide/topics/ui/settings/use-saved-values
 
 //        rangeSlider.addOnSliderTouchListener(object: RangeSlider.OnSliderTouchListener{
 //        @SuppressLint("LongLogTag")
