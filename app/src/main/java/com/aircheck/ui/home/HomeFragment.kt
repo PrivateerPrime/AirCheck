e316472ec1f45e6ac5e065d63992f72bbee2a881
PrivateerPrime <kacperlipowicz@gmail.com>
2021-12-13 15:25:46 +0100

    Dodano wyświetlanie aktualnych oraz prognozowanych danych

--- app/src/main/java/com/aircheck/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aircheck/ui/home/HomeFragment.kt
@@ -1,19 +1,18 @@
 package com.aircheck.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.aircheck.R
 import com.aircheck.databinding.FragmentHomeBinding
+import com.google.android.material.slider.RangeSlider
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+//    private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -25,30 +24,43 @@ class HomeFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+//        homeViewModel =
+//                ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        val preferences = activity?.getPreferences(Context.MODE_PRIVATE)
+        val editor = preferences?.edit()
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        val textView2: TextView = binding.textHome2
-        val textView3: TextView = binding.textHome3
-        val textView4: TextView = binding.textHome4
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
 
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        homeViewModel.text2.observe(viewLifecycleOwner, Observer {
-            textView2.text = it
-        })
-        homeViewModel.text3.observe(viewLifecycleOwner, Observer {
-            textView3.text = it
-        })
-        homeViewModel.text4.observe(viewLifecycleOwner, Observer {
-            textView4.text = it
-        })
-        return root
+        val textPollution: TextView = binding.textPollution
+        val textTemperature: TextView = binding.textTemperature
+        val textHumidity: TextView = binding.textHumidity
+        val textPressure: TextView = binding.textPressure
+
+        val hour = preferences?.getFloat("forecastRange", 0F)?.toInt()
+        textPollution.text = preferences?.getString("pollutionMain$hour", "NODATA")
+        textTemperature.text = preferences?.getString("temperatureMain$hour", "NODATA")
+        textHumidity.text = preferences?.getString("humidityMain$hour", "NODATA")
+        textPressure.text = preferences?.getString("pressureMain$hour", "NODATA")
+
+        val rangeSlider: RangeSlider = binding.sliderHome
+        rangeSlider.values = listOf(preferences?.getFloat("forecastRange", 0F))
+        rangeSlider.addOnChangeListener {
+                _, _, _ ->
+            val values = rangeSlider.values
+            editor?.putFloat("forecastRange", values[0])
+            editor?.apply()
+            val hourTemp = values[0].toInt()
+            textPollution.text = preferences?.getString("pollutionMain$hourTemp", "NODATA")
+            textTemperature.text = preferences?.getString("temperatureMain$hourTemp", "NODATA")
+            textHumidity.text = preferences?.getString("humidityMain$hourTemp", "NODATA")
+            textPressure.text = preferences?.getString("pressureMain$hourTemp", "NODATA")
+        }
+        return binding.root
     }
 
     override fun onDestroyView() {
