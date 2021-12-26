bf8867a10ba53de564eb237ecd8c2bdb70144cc4
PrivateerPrime <kacperlipowicz@gmail.com>
2021-12-26 23:07:29 +0100

    Nowe Data Class

---
+++ app/src/main/java/com/aircheck/MetricsDataClass.kt
@@ -0,0 +1,70 @@
+package com.aircheck
+
+data class MetricsDataClass(
+    val current: Current,
+    val hourly: List<Hourly>,
+    val lat: Double,
+    val lon: Double,
+    val timezone: String,
+    val timezone_offset: Int
+)
+
+data class Current(
+    val clouds: Int,
+    val dew_point: Double,
+    val dt: Int,
+    val feels_like: Double,
+    val humidity: Double,
+    val pressure: Int,
+    val sunrise: Int,
+    val sunset: Int,
+    val temp: Double,
+    val uvi: Double,
+    val visibility: Int,
+    val weather: List<Weather>,
+    val wind_deg: Int,
+    val rain: Rain,
+    val snow: Snow,
+    val wind_speed: Double
+)
+
+data class Hourly(
+    val clouds: Int,
+    val dew_point: Double,
+    val dt: Int,
+    val feels_like: Double,
+    val humidity: Double,
+    val pop: Double,
+    val pressure: Int,
+    val rain: Rain,
+    val snow: Snow,
+    val temp: Double,
+    val uvi: Double,
+    val visibility: Int,
+    val weather: List<WeatherX>,
+    val wind_deg: Int,
+    val wind_gust: Double,
+    val wind_speed: Double
+)
+
+data class Weather(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
+
+data class Rain(
+    val `1h`: Double
+)
+
+data class Snow(
+    val `1h`: Double
+)
+
+data class WeatherX(
+    val description: String,
+    val icon: String,
+    val id: Int,
+    val main: String
+)
