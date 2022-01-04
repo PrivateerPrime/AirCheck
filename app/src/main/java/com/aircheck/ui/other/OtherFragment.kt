2b56eea90e547e407f1f72c21047ad801ddc1319
PrivateerPrime <kacperlipowicz@gmail.com>
2022-01-04 14:53:33 +0100

    Przygotowanie pod nowy widok

--- app/src/main/java/com/aircheck/ui/other/OtherFragment.kt
+++ app/src/main/java/com/aircheck/ui/other/OtherFragment.kt
@@ -1,32 +1,37 @@
 package com.aircheck.ui.other
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.aircheck.R
 import com.aircheck.databinding.FragmentOtherBinding
 
 class OtherFragment : Fragment() {
 
-    private lateinit var otherViewModel: OtherViewModel
+    //private lateinit var otherViewModel: OtherViewModel
     private var _binding: FragmentOtherBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.top_app_bar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        otherViewModel =
-                ViewModelProvider(this).get(OtherViewModel::class.java)
+//        otherViewModel =
+//                ViewModelProvider(this).get(OtherViewModel::class.java)
 
+        super.onCreateView(inflater, container, savedInstanceState)
+        setHasOptionsMenu(true)
         _binding = FragmentOtherBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
